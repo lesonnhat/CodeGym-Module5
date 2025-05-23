53e1edfdb047e0e8c9c92cbf4f8f1499c43c1e1f
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-05-23 11:08:55 +0700

    add module 5

---
+++ Week1/Lesson1/demo/index.ts
@@ -0,0 +1,15 @@
+let x: number = 1;
+let y: number = 9;
+
+if (x > y)
+{
+    console.log('x lớn hơn y.');
+}
+else if (x < y)
+{
+    console.log('x nhỏ hơn y.');
+}
+else if (x == y)
+{
+    console.log('x bằng y');
+}
