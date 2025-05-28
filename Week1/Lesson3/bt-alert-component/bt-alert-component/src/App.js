ea04c79dcfae13cf7e2376621e66e51d13b4c935
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-05-28 14:37:47 +0700

    add bt alert component

---
+++ Week1/Lesson3/bt-alert-component/bt-alert-component/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="alert alert-warning" role="alert">
+          Cảnh báo! Tài nguyên bạn vừa truy cập không tồn tại.
+        </div>
+    )
+}
+
+export default App;
